77b430bf81c23b02ac4cf57afc2983ea0d7fa43e
Ares <Ares@DESKTOP-DSOCML1>
2016-03-16 23:00:41 -0400

    Finished MergeSort

--- SoringAlgorithms/src/Main.java
+++ SoringAlgorithms/src/Main.java
@@ -1,20 +1,53 @@
+import java.util.Arrays;
 
 public class Main {
 
 	public static void main(String[] args) {
-		int[] array = {3,5,8,3,2,7,8,4,3,5,2,10,12,3,5,23};
+		int[] array = {11,9,8,7,6,23,5,34,4,3,2,12,1,45,0,12};
 		
 		printArray(array);
-		selectionSort(array);
+		array = mergeSort(array);
 		printArray(array);
 		
 	}
 
 	public static void printArray(int[] inputArray){
+		if(inputArray == null){return;}
 		for(int a : inputArray){
 			System.out.print(a+",");
 		}
 		System.out.println("");
+	}
+	
+	public static int[] mergeSort(int[] inputArray){
+		int[] a;
+		int[] b;
+		int[] output = new int[inputArray.length];
+		if(inputArray.length>1){
+			a = mergeSort(Arrays.copyOfRange(inputArray, 0, (inputArray.length/2)));
+			b = mergeSort(Arrays.copyOfRange(inputArray, (inputArray.length/2),inputArray.length));
+		}else{
+			return inputArray;
+		}
+		//merge
+		int i=0;
+		int j=0;
+		while(i < a.length || j < b.length){
+			if(i == a.length){
+				output[i+j] = b[j];
+				j++;
+			}else if(j == b.length){
+				output[i+j] = a[i];
+				i++;
+			}else if(a[i]>b[j]){
+				output[i+j] = b[j];
+				j++;
+			}else{
+				output[i+j] = a[i];
+				i++;
+			}
+		}
+		return output;
 	}
 	
 	public static void selectionSort(int[] inputArray){
