d7b32667ef4f0e85e2bb9903be8ae0b5f5a84498
William Dullemond <Dullemond.William@gmail.com>
2016-07-22 21:36:03 -0400

    Implemented Testing
    Added Separate case statements for each algorithm

---
+++ SoringAlgorithms/src/Testing/AlgorithmTesting.java
@@ -0,0 +1,58 @@
+package Testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import alogrithms.*;
+
+public class AlgorithmTesting {
+	int[] array = {10,2,1,4,6,3,9,3,1,8,4,12,15};
+	SortingAlgorithm sorting;
+	
+	public Boolean inOrder(int[] array){
+		for(int i=1; i < array.length; i++){
+			if(array[i]<array[i-1]){
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	
+	@Test
+	public void testBubbleSort() {
+		sorting = new BubbleSort();
+		assertTrue(inOrder(sorting.sort(array)));
+	}
+	
+	@Test
+	public void testHeapSort() {
+		sorting = new HeapSort();
+		assertTrue(inOrder(sorting.sort(array)));
+	}
+	
+	@Test
+	public void testBubbleInsertionSort() {
+		sorting = new InsertionSort();
+		assertTrue(inOrder(sorting.sort(array)));
+	}
+	
+	@Test
+	public void testMergeSort() {
+		sorting = new MergeSort();
+		assertTrue(inOrder(sorting.sort(array)));
+	}
+	
+	@Test
+	public void testQuickSort() {
+		sorting = new QuickSort();
+		assertTrue(inOrder(sorting.sort(array)));
+	}
+	
+	@Test
+	public void testSelectionSort() {
+		sorting = new SelectionSort();
+		assertTrue(inOrder(sorting.sort(array)));
+	}
+}
